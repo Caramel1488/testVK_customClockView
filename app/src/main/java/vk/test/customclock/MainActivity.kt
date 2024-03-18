0ebccb63a40126a2eaa73b6546634b11ecf17ce6
Vladimir <vova_klushin@mail.ru>
2024-03-18 17:23:27 +0300

    add clock view problem with xml attrs

--- app/src/main/java/vk/test/customclock/MainActivity.kt
+++ app/src/main/java/vk/test/customclock/MainActivity.kt
@@ -2,10 +2,9 @@ package vk.test.customclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
+import java.util.Calendar
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
